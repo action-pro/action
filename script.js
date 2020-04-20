a9f686bb76e78a7b2f9b0a445fc9ed4a649014d7
action-pro <64007923+action-pro@users.noreply.github.com>
2020-04-20 12:10:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 jQuery(document).ready(function(){
 	jQuery.getJSON('https://ipapi.co/json/',function(data){ 
 		console.log(JSON.stringify(data,null,2));
+		/*
 		var parsed = JSON.parse(JSON.stringify(data,null,2));
 		if(parsed.country!='US'){
 			setTimeout(function(){window.location.replace(location.host);},1000); 
 		}
+		*/
 	});
 });
